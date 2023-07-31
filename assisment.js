e32ae98e6fe2db4fd9a2fbbcd5e79a1a15af3178
Amit Kumar <112743124+Lucretia37@users.noreply.github.com>
2023-07-31 09:17:49 +0530

    Update assisment.js

--- assisment.js
+++ assisment.js
@@ -1,28 +1,16 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
-
 // create a variable to hold your NFT's
-const NFTs = []
+const NFTs = [];
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT(_name, _hairColour, _topWear, _accessory) {
-	
+function mintNFT(_name, _Age, _Hobby, _FavMovie) {
 	const NFT = {
-		"name" : _name,
-		"hairColour" : _hairColour,
-		"topWear" : _topWear,
-		"accessory" : _accessory
-
-	}
+		"name": _name,
+		"Age": _Age,
+		"Hobby": _Hobby,
+		"FavMovie": _FavMovie
+	};
 	NFTs.push(NFT);
 	console.log("Minted: " + _name);
 }
@@ -30,25 +18,24 @@ function mintNFT(_name, _hairColour, _topWear, _accessory) {
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs() {
-	for(let i = 0;i<NFTs.length;i++) {
-		console.log("\nname: "+NFTs[i].name);
-		console.log("hairColour: "+NFTs[i].hairColour);
-		console.log("topWear: "+NFTs[i].topWear);
-		console.log("accessory: "+NFTs[i].accessory);
+	for (let i = 0; i < NFTs.length; i++) {
+		console.log("\nname: " + NFTs[i].name);
+		console.log("Age: " + NFTs[i].Age);
+		console.log("Hobby: " + NFTs[i].Hobby);
+		console.log("FavMovie: " + NFTs[i].FavMovie);
 	}
-
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-	console.log("Total Supply: "+NFTs.length);
+	console.log("Total Supply: " + NFTs.length);
 }
 
 // call your functions below this line
-mintNFT("Jack","23","Gardning");
-mintNFT("Skyla","21","Coading");
-mintNFT("Amit","21","Football");
-mintNFT("Uzzi","30","Painting");
+mintNFT("Jack", "23", "Gardning","Avatar");
+mintNFT("Skyla", "21", "Coading","Avengers");
+mintNFT("Amit", "21", "Football","Inception");
+mintNFT("Uzzi", "30", "Painting","Wall-E");
 listNFTs();
 console.log("\n");
 getTotalSupply();
